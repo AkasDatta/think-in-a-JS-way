a812cf4551b0426ac6025998deac62ac3d2c31b9
Akas_Datta <012akasdatta@gmail.com>
2024-03-12 18:05:48 +0600

    variable done

--- variable/variable.js
+++ variable/variable.js
@@ -1,14 +1,31 @@
-var varVariable = "This is var";
+// var varVariable = "This is var";
 
+// if(true){
+//     var varVariable = "This is var again";
+// }
+
+// console.log(varVariable);
+
+/* let
 if(true){
-    var varVariable = "This is var again";
+    let letVariable = "This is let";
+    let letVariable = "This is let again";
 }
-
-console.log(varVariable);
+console.log(letVariable);
 
 
 if(true){
     let letVariable = "This is let";
+    letVariable = "This is let again";
+}
+console.log(letVariable);
+*/
+
+/* const
+if(true){
+    const constVariable = "This is const";
+    constVariable = "This is const again";
 }
 
-console.log(letVariable);
+console.log(constVariable);
+*/
